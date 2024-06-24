321fc27ff58642e00044e721649f02a58eabc7e7
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-06-24 21:15:57 +0900

    3.4.1 등록 기능의 구현

--- src/main/java/org/zerock/mallapi/service/TodoServiceImpl.java
+++ src/main/java/org/zerock/mallapi/service/TodoServiceImpl.java
@@ -1,22 +1,34 @@
 package org.zerock.mallapi.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mallapi.domain.Todo;
 import org.zerock.mallapi.dto.TodoDTO;
+import org.zerock.mallapi.repository.TodoRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Transactional
 @Log4j2
+@RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService{
+
+    private final ModelMapper modelMapper;
+    private final TodoRepository todoRepository;
 
     @Override
     public Long register(TodoDTO todoDTO) {
         
        log.info(".........."); 
 
-       return null;
+        Todo todo = modelMapper.map(todoDTO, Todo.class);
+
+        Todo savedTodo = todoRepository.save(todo);
+
+       return savedTodo.getTno();
     }
     
 }
