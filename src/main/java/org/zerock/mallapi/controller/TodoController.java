0ef1571ac49f9f1c60f71d07508545a5ed9a7fed
unknown <leegj420@gmail.com>
2024-06-25 10:38:18 +0900

    3.7.4 DELETE 방식의 삭제 처리

--- src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -12,6 +12,7 @@ import lombok.extern.log4j.Log4j2;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,16 @@ public class TodoController {
         log.info("Modify: " + todoDTO);
 
         service.modify(todoDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+
+        log.info("Remove: " + tno);
+
+        service.remove(tno);
 
         return Map.of("RESULT", "SUCCESS");
     }
