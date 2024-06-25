bd317d44e2907d496c2198ccb2706e83f0e8bf76
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-06-26 00:00:34 +0900

    5.2 컨트롤러에서의 파일 처리

---
+++ src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -0,0 +1,40 @@
+package org.zerock.mallapi.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mallapi.dto.ProductDTO;
+import org.zerock.mallapi.util.CustomFileUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/products")
+public class ProductController {
+    
+    // 신기하게 네이밍이 service가 아니라 util이다
+    private final CustomFileUtil fileUtil;
+
+    @PostMapping("/")
+    public Map<String, String> register(ProductDTO productDTO){
+
+        log.info("register: " + productDTO);
+
+        List<MultipartFile> files = productDTO.getFiles();
+
+        List<String> uploadFileNames = fileUtil.saveFiles(files);
+
+        productDTO.setUploadFileNames(uploadFileNames);
+
+        log.info(uploadFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+}
