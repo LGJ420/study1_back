91d47a20547049b32e84048d5d8e4240f0ae1c56
unknown <leegj420@gmail.com>
2024-06-25 10:41:30 +0900

    3.7.5 CORS관련 설정

--- src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
+++ src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -2,6 +2,7 @@ package org.zerock.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.mallapi.controller.formatter.LocalDateFomatter;
 
@@ -12,5 +13,15 @@ public class CustomServletConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
 
         registry.addFormatter(new LocalDateFomatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .maxAge(300)
+            .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
     }
 }
