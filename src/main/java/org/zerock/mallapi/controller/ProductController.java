0920bb5ea2c9b482e1bbed273d88bb1c2fcd4d22
unknown <leegj420@gmail.com>
2024-06-26 13:01:19 +0900

    5.4.1 목록 기능의 처리

--- src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mallapi.dto.PageRequestDTO;
+import org.zerock.mallapi.dto.PageResponseDTO;
 import org.zerock.mallapi.dto.ProductDTO;
+import org.zerock.mallapi.service.ProductService;
 import org.zerock.mallapi.util.CustomFileUtil;
 
 import lombok.RequiredArgsConstructor;
@@ -25,6 +28,17 @@ public class ProductController {
     
     // 신기하게 네이밍이 service가 아니라 util이다
     private final CustomFileUtil fileUtil;
+
+    // 알고보니 서비스가 있었음, util은 파일 저장 기능이었던것
+    private final ProductService productService;
+
+    @GetMapping("/list")
+    public PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO){
+
+        log.info("list............" + pageRequestDTO);
+
+        return productService.getList(pageRequestDTO);
+    }
 
     @PostMapping("/")
     public Map<String, String> register(ProductDTO productDTO){
