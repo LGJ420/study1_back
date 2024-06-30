26043e7bbe062de369deec23a6948c75a14a9037
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-06-30 19:16:14 +0900

    9.4.2 SocialController의 개발

---
+++ src/main/java/org/zerock/mallapi/controller/SocialController.java
@@ -0,0 +1,28 @@
+package org.zerock.mallapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.mallapi.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class SocialController {
+    
+    private final MemberService memberService;
+
+    @GetMapping("/api/member/kakao")
+    public String[] getMemberFromKakao(String accessToken){
+
+        log.info("access Token ");
+        log.info(accessToken);
+
+        memberService.getKakaoMember(accessToken);
+
+        return new String[]{"AAA", "BBB", "CCC"};
+    }
+
+}
